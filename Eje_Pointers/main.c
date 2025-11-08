99c46c555e92c51e75ddaa333fe1a97a5c760a78
DrPool16 <elmesdepool@gmail.com>
2025-11-07 21:54:13 -0500

    Exercice 5

--- Eje_Pointers/main.c
+++ Eje_Pointers/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+int Ejercicio5(int *p5);
 int main()
 {
-    printf("Hello world!\n");
+    int var5 = 5;
+    printf("Initial value of var5: %d \n", var5);
+    int *p5_initial = &var5;
+    Ejercicio5(p5_initial);
+    printf("New value of var5: %d \n", var5);
     return 0;
+}
+
+int Ejercicio5(int *p5)
+
+    *p5 = 15
+    return *p;
 }
